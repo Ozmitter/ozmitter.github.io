b657be4461ca0f0e7b50d33945b2f2c6eb104fea
Austin <11578010+Ozmitter@users.noreply.github.com>
2022-07-09 03:03:27 -0400

    Update test.js

--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -1,6 +1,7 @@
 var navHeaderC = new Boolean(false);
 
 function onNavHeaderClick(pressedTab) {
+	console.log(pressedTab);
 	document.getElementById("particles-js").remove();
 }
 
@@ -12,7 +13,7 @@ window.onload = function test() {
     	document.getElementsByClassName("container")[0].append(newHdiv);
     	document.getElementsByClassName("container")[0].setAttribute("id","containerone");
 	
-    	document.getElementsByClassName("nav--header")[0].setAttribute("onclick","onNavHeaderClick()");
+    	document.getElementsByClassName("nav--header")[0].setAttribute("onclick","onNavHeaderClick(this)");
 		
 	particlesJS("particles-js", 
 		{
